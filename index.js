f992de6d4e0fce879ff849c4e2b93098eb6e0168
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-02-22 16:05:12 -0500

    basic setup for google oauth

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
-// import express from "express";
-
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
