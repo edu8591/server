8b922e12b9c5919b01d09fc244dc83ab30c0dfd8
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-03-07 14:12:22 -0500

    reorder imports on indexjs to fix user creation error, create user on googleAuth if user doesnt exists

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("./services/passport");
 const keys = require("./config/keys");
-const authRoutes = require("./routes/authRoutes");
 require("./models/User");
+require("./services/passport");
+const authRoutes = require("./routes/authRoutes");
 
 mongoose.connect(keys.mongoURI);
 const app = express();
