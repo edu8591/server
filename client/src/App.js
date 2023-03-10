bc47d7b08f41429bf15a09329569b5c6eb09a7b3
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-03-10 14:23:51 -0500

    remove client boilerplate and install react-router, redux, react-redux, toolkit dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="/auth/google">Sign in with Google</a>
-      </header>
-    </div>
-  );
+  return <div>App component</div>;
 }
 
 export default App;
