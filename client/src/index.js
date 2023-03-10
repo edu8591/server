d8bf6c535130f9425a376b835b077a9d82cac38f
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-03-10 15:28:41 -0500

    Created redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const rootEl = document.getElementById("root");
 const root = ReactDOM.createRoot(rootEl);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
